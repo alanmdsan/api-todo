cc71558f6304b9e130046f0de33ea4f65135a763
alanmdsan <alanmdsan@gmail.com>
2023-11-25 22:32:00 -0300

    setting put and delete methods

--- src/routes.js
+++ src/routes.js
@@ -10,11 +10,13 @@ const Note = mongoose.model('Note', {
   color: String
 })
 
+// GET
 todoRoutes.get('/todos', async (req, res) => {
   const notes = await Note.find()
   return res.status(200).json(notes)
 })
 
+// POST
 todoRoutes.post('/todos', async (req, res) => {
   const { title, description, favorite, color } = req.body
   const note = new Note({
@@ -25,6 +27,30 @@ todoRoutes.post('/todos', async (req, res) => {
   })
   await note.save()
   return res.status(201).json(note)
+})
+
+// PUT
+todoRoutes.put('/todos/:id', async (req, res) => {
+  const { title, description, favorite, color } = req.body
+  const note = await Note.findByIdAndUpdate(
+    req.params.id,
+    {
+      title,
+      description,
+      favorite,
+      color
+    },
+    {
+      new: true
+    }
+  )
+  return res.status(200).json(note)
+})
+
+// DELETE
+todoRoutes.delete('/todos/:id', async (req, res) => {
+  const note = await Note.findByIdAndDelete(req.params.id)
+  return res.status(200).json(note)
 })
 
 module.exports = todoRoutes
